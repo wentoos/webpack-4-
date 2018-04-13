d119d3a42b58c7da81ce3891dc410066ab3ee0f2
wentoo <1217167967@qq.com>
2018-04-13 17:07:02 +0800

    ok

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -11,16 +11,23 @@ const happyThreadPool = HappyPack.ThreadPool({
 
 module.exports = {
     entry: './src/app.tsx',
+
     vendors: [
-        'react', 'react-dom', 'react-router', 'react-redux', 'redux', 'redux-thunk', 'react-transition-group', 'prop-types', 'swiper', 'fastclick',
+        'react', 'react-dom', '@types/react-dom', "@types/react", 'react-router', 'react-redux', 'redux', 'redux-thunk', 'react-transition-group', 'prop-types', 'swiper', 'fastclick',
     ],
     devtool: 'inline-source-map',//指向错误代码
     output: {
         path: path.resolve("", '/dist'),
-        filename: 'js/bundle.[hash:8].js'
+        filename: 'js/bundle.[hash:8].js',
+        chunkFilename: 'js/[name].[hash:8].min.js',
     },
     resolve: {
-        extensions: [".ts", ".tsx", ".js",".css", ".less", ".json"]
+        extensions: [".ts", ".tsx", ".js", ".css", ".less", ".json"]
+    },
+    optimization: {
+        splitChunks: {
+            name: 'vendors',
+        }
     },
     module: {
         rules: [
@@ -38,24 +45,24 @@ module.exports = {
                 //注意后面那个limit的参数，当你图片大小小于这个限制的时候，会自动启用base64编码图片(只能是相对路径图片编码background:url(../img/file.png))
                 use: ['url-loader?limit=8192&name=img/[hash:8].[name].[ext]']
             },
-            { 
+            {
                 test: /\.(eot|woff|svg|ttf|woff2|gif|appcache)(\?|$)/,
                 exclude: /^node_modules$/,
                 use: ['file-loader?name=[name].[ext]']
-            }, 
+            },
             {
                 test: /\.svg/,
                 loader: 'svg-url-loader?limit=8192&name=images/[hash:8].[name].[ext]'
             },
-            { 
-                test: /\.tsx?$/, 
-                exclude: /node_modules/, 
-                loader: "babel-loader!ts-loader" 
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader!ts-loader"
             },
-            { 
-                test: /\.js$/, 
-                exclude: /node_modules/, 
-                use: 'babel-loader' 
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
             }
         ]
     },
@@ -70,10 +77,6 @@ module.exports = {
         }),
         new webpack.HotModuleReplacementPlugin(),// 开启全局的模块热替换(HMR)
         new webpack.NamedModulesPlugin(),// 当模块热替换(HMR)时在浏览器控制台输出对用户更友好的模块名字信息
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendors',
-            filename: 'js/vendors.js'
-        }), 
         new OpenBrowserPlugin({
             //自动打开浏览器
             url: 'http://localhost:8000/',
